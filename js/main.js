a115d6e469b566007085177e8f036675cdcafe1d
Alley Manalio <amanalio@ufl.edu>
2016-07-13 23:33:11 -0400

    Add beginnings of the second event listener

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ function serious() {
 }
 
 bridgette.addEventListener('click', serious);
+
+//Event #2 - When you keydown, the tagline "Expect the unexpected!" appears beneath the h1 tag.
+
+var header = document.getElementById('header');
+function tagline() {
+    var tag = document.createElement('article');
+    tag.innerHTML = '<p>Expect the unexpected!</p>';
+    document.getElementsByClassName('header').appendChild(tag);
+}
+
+header.addEventListener('keydown', tagline);
