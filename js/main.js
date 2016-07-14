67dc35df712683cb214831475775eaeaf5cb43e4
Alley Manalio <amanalio@ufl.edu>
2016-07-14 17:38:42 -0400

    Add second event listener

--- js/main.js
+++ js/main.js
@@ -7,13 +7,27 @@ function serious() {
 
 bridgette.addEventListener('click', serious);
 
-//Event #2 - When you keydown, the tagline "Expect the unexpected!" appears beneath the h1 tag.
+//Event #2 - A double-click event listener is set on the last paragraph on the page. When double-clicked, a section tag is added with a funny gif and a few paragraphs.
 
-var header = document.getElementById('header');
-function tagline() {
-    var tag = document.createElement('article');
-    tag.innerHTML = '<p>Expect the unexpected!</p>';
-    document.getElementsByClassName('header').appendChild(tag);
+var warning = document.getElementById('warning');
+
+function veryserious() {
+    var theImage = document.createElement('section');
+
+    theImage.innerHTML = '<h2>Okay, one more thought!</h2> <p>Me as I watch Frank make a complete fool out of Bridgette for all of America to see:</p> <img src="img/AubreyPlazaNo.gif" alt="My face when I hear her talk." /> <p>Honey, it\'s time for you to GO.</p>';
+
+    document.getElementsByClassName('container')[0].appendChild(theImage);
 }
 
-header.addEventListener('keydown', tagline);
+warning.addEventListener('dblclick', veryserious);
+
+
+
+// var header = document.getElementById('header');
+// function tagline() {
+//     var tag = document.createElement('article');
+//     tag.innerHTML = '<p>Expect the unexpected!</p>';
+//     document.getElementsByClassName('header').appendChild(tag);
+// }
+
+// header.addEventListener('keydown', tagline);
